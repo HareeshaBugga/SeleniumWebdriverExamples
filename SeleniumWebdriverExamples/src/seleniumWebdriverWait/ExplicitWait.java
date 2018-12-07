c9ee7db67ea1a935e59a9333d560c30b4639051c
Hareesha Bugga <Hareesha.Bugga@dal.ca>
2018-12-07 16:39:50 -0400

    Example of explicit wait.

---
+++ SeleniumWebdriverExamples/src/seleniumWebdriverWait/ExplicitWait.java
@@ -0,0 +1,26 @@
+package seleniumWebdriverWait;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "give the path of chrome in your desktop" );
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://seleniumpractise.blogspot.in/2...");
+		driver.manage().window().maximize();  //to maximize
+		
+		WebDriverWait wait = new WebDriverWait(driver,20);
+		WebElement aboutme;
+		aboutme =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"site-navigationpbt\"]/div/ul/li[2]/a")));
+		aboutme.click();
+		driver.quit();
+			
+	}
+
+}

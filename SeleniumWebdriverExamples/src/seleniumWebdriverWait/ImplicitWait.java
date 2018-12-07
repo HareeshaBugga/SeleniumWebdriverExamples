555bf1e84960ea44411f45d4ad7a471db10e14ff
Hareesha Bugga <Hareesha.Bugga@dal.ca>
2018-12-07 16:19:24 -0400

    changed path

--- SeleniumWebdriverExamples/src/seleniumWebdriverWait/ImplicitWait.java
+++ SeleniumWebdriverExamples/src/seleniumWebdriverWait/ImplicitWait.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ImplicitWait {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\haree\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe" );
+		System.setProperty("webdriver.chrome.driver", "give the path of chrome in your desktop" );
 		ChromeDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.mortgagecalculator.org/");

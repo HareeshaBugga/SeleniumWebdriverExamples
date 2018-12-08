81bf8596e06c4cbbc8b6c6652b3c354f4a640211
Hareesha Bugga <Hareesha.Bugga@dal.ca>
2018-12-07 21:28:01 -0400

    upload a file

--- SeleniumWebdriverExamples/src/handlingAlertsAndPopUp/BrowserWindowPopUp.java
+++ SeleniumWebdriverExamples/src/handlingAlertsAndPopUp/BrowserWindowPopUp.java
@@ -12,7 +12,7 @@ public class BrowserWindowPopUp {
 	public static void main(String[] args) {
 		
 	WebDriver driver;
-	System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\haree\\\\Desktop\\\\selenium\\\\chromedriver_win32\\\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "give path of chrome driver");
 	driver = new ChromeDriver();
 	
 	driver.get("http://popuptest.com/goodpopups.html");

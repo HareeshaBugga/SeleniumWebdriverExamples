85c6fe5ac70cf569e25a3f5cc386d0a236abe34c
Hareesha Bugga <Hareesha.Bugga@dal.ca>
2018-12-07 20:57:29 -0400

    Handling file upload popup

---
+++ SeleniumWebdriverExamples/src/handlingAlertsAndPopUp/HandleFileUploadPopup.java
@@ -0,0 +1,25 @@
+package handlingAlertsAndPopUp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleFileUploadPopup {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "enter path of chrome driver");
+		driver = new ChromeDriver();
+		
+		driver.get("https://html.com/input-type-file/");
+		driver.manage().window().maximize(); 
+		
+		WebElement fileupload = driver.findElement(By.id("fileupload"));
+		fileupload.sendKeys("give a filepath from your desktop");
+		driver.quit();
+		
+
+	}
+
+}

3253014e56843979e642a1bb116264ac0d2d51cd
Hareesha Bugga <Hareesha.Bugga@dal.ca>
2018-12-10 19:00:47 -0400

    Revert "dynamic search"
    This reverts commit 7b0a059ed21528d5c4832e9f2c52de47400f1b6d.

--- SeleniumWebdriverExamples/src/HTMLUnitdriver/BrowsertestingUsingHtmlUnitdriver.java
+++ SeleniumWebdriverExamples/src/HTMLUnitdriver/BrowsertestingUsingHtmlUnitdriver.java
@@ -1,20 +1,20 @@
-//package HTMLUnitdriver;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-//
-//public class BrowsertestingUsingHtmlUnitdriver {
-//
-//public static void main(String[] args) {
-//		WebDriver driver = new HtmlUnitDriver();
-//		
-//		driver.get("https://www.google.com");
-//		driver.manage().window().maximize();  //to maximize
-//		driver.manage().deleteAllCookies();
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//		System.out.println(driver.getTitle());
-//	}
-//
-//}
+package HTMLUnitdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class BrowsertestingUsingHtmlUnitdriver {
+
+public static void main(String[] args) {
+		WebDriver driver = new HtmlUnitDriver();
+		
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();  //to maximize
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+	}
+
+}
